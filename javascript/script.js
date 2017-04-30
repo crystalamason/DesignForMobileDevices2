495563a27d319d20ba298e8af79fb6646394a6be
crystalamason <m011220c@student.staffs.ac.uk>
2017-04-30 20:29:33 +0100

    Changing zoom on google maps

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,7 @@
 function initMap() {
     var uluru = {lat: 52.803616, lng: -2.115849400000002};
     var map = new google.maps.Map(document.getElementById('mapStafford'), {
-          zoom: 4,
+          zoom: 2,
           center: uluru
     });
     var marker = new google.maps.Marker({
