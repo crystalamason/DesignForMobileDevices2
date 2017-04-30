61e1ae205a0886e8a0399d8a79463a94fbb60743
crystalamason <m011220c@student.staffs.ac.uk>
2017-04-30 20:03:37 +0100

    Adding google maps to stafford cinema page

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,13 @@
 //JavaScript
+
+function initMap() {
+    var uluru = {lat: 52.803616, lng: -2.115849400000002};
+    var map = new google.maps.Map(document.getElementById('mapStafford'), {
+          zoom: 4,
+          center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
