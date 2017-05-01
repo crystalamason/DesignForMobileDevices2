b91557be0924285755f2d4adecfcca51bc333757
crystalamason <m011220c@student.staffs.ac.uk>
2017-05-01 17:44:54 +0100

    Changing size of image

--- javascript/script.js
+++ javascript/script.js
@@ -6,7 +6,7 @@ function initMap() {
           zoom: 17,
           center: uluru
     });
-    var image = "images/filmReel.png";
+    var image = "images/googleMarker.png";
     var marker = new google.maps.Marker({
         position: uluru,
         map: map,
