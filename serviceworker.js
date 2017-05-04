01e0cd50c8a7ccf3b7805b3280016b5a12a04148
crystalamason <m011220c@student.staffs.ac.uk>
2017-05-04 16:16:50 +0100

    Trying things out with the service worker

--- serviceworker.js
+++ serviceworker.js
@@ -5,3 +5,15 @@ if ('serviceWorker' in navigator) {
           console.log('Service worker registration failed: ', err);
         });
 }
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return new Response(
+        'Welcome to the our service worker test \n'+        
+'There seems to be a problem with your connection.\n'+        
+'We look forward to telling you about Paddy as soon as you are online'
+      );
+    })
+  );
+});
