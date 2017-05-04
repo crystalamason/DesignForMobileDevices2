24e760408a91a392e19b275aff705d34130cb691
crystalamason <m011220c@student.staffs.ac.uk>
2017-05-04 17:32:58 +0100

    Changing service worker code

--- serviceworker.js
+++ serviceworker.js
@@ -10,9 +10,9 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
       return new Response(
-        'Welcome to the our service worker test \n'+        
+        'Welcome to Reel \n'+        
 'There seems to be a problem with your connection.\n'+        
-'We look forward to telling you about Paddy as soon as you are online'
+'We look forward to telling you about the latest films as soon as you are online'
       );
     })
   );
