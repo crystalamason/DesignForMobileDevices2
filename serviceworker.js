e25bbcc15cd204f4dc91bd6e4d68fbcd9c8126f8
crystalamason <m011220c@student.staffs.ac.uk>
2017-05-05 12:33:01 +0100

    Adding caching

--- serviceworker.js
+++ serviceworker.js
@@ -6,17 +6,17 @@ if ('serviceWorker' in navigator) {
         });
 }
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return new Response(
-        'Welcome to Reel \n'+        
-'There seems to be a problem with your connection.\n'+        
-'We look forward to telling you about the latest films as soon as you are online'
-      );
-    })
-  );
-});
+//self.addEventListener('fetch', function(event) {
+//  event.respondWith(
+//    fetch(event.request).catch(function() {
+//      return new Response(
+//        'Welcome to Reel \n'+        
+//'There seems to be a problem with your connection.\n'+        
+//'We look forward to telling you about the latest films as soon as you are online'
+//      );
+//    })
+//  );
+//});
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
