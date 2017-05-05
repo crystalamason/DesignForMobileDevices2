fafa7733eb1872a72977a10531cd8602f27092ec
crystalamason <m011220c@student.staffs.ac.uk>
2017-05-05 12:57:04 +0100

    Adding code for caching

--- serviceworker.js
+++ serviceworker.js
@@ -12,10 +12,32 @@
 //  );
 //});
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match('index-offline.html');
+var CACHE_NAME = 'gih-cache';
+var CACHED_URLS = [
+    'index-offline.html',
+    'css/bulma.css',
+    'css/styles.css'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
     })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('index-offline.html');
+        }
+      });
+    })
+  );
+});
+
