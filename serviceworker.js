ae5c784cb87d9377e9f2614304a8e0f6fe602bfc
crystalamason <m011220c@student.staffs.ac.uk>
2017-05-07 13:31:17 +0100

    Adding in caching

--- serviceworker.js
+++ serviceworker.js
@@ -12,11 +12,53 @@
 //  );
 //});
 
+var BASE_PATH = '/DesignForMobileDevices2/';
 var CACHE_NAME = 'gih-cache';
+var TEMP_IMAGE_CACHE_NAME = 'temp-cache-v1'
 var CACHED_URLS = [
-    'index-offline.html',
-    'css/bulma.css',
-    'css/styles.css'
+    //Our HTML
+    BASE_PATH + 'index.html',
+    
+    //Images for favicons
+    BASE_PATH + 'images/favicon/android-icon-36x36.png',
+    BASE_PATH + 'images/favicon/images/favicon/android-icon-48x48.png',
+    BASE_PATH + 'images/favicon/android-icon-72x72.png',
+    BASE_PATH + 'images/favicon/android-icon-96x96.png',
+    BASE_PATH + 'images/favicon/android-icon-144x144.png',
+    BASE_PATH + 'images/favicon/android-icon-192x192.png',
+    BASE_PATH + 'images/favicon/apple-icon',
+    BASE_PATH + 'images/favicon/apple-icon-57x57.png',
+    BASE_PATH + 'images/favicon/apple-icon-60x60.png',
+    BASE_PATH + 'images/favicon/apple-icon-72x72.png',
+    BASE_PATH + 'images/favicon/apple-icon-76x76.png',
+    BASE_PATH + 'images/favicon/apple-icon-114x114.png',
+    BASE_PATH + 'images/favicon/apple-icon-120x120.png',
+    BASE_PATH + 'images/favicon/apple-icon-144x144.png',
+    BASE_PATH + 'images/favicon/apple-icon-152x152.png',
+    BASE_PATH + 'images/favicon/apple-icon-180x180.png',
+    BASE_PATH + 'images/favicon/apple-icon-preco-mposed',
+    BASE_PATH + 'images/favicon/favicon.png',
+    BASE_PATH + 'images/favicon/favicon-16x16.png',
+    BASE_PATH + 'images/favicon/favicon-32x32.png',
+    BASE_PATH + 'images/favicon/favicon-96x96.png',
+    BASE_PATH + 'images/favicon/ms-icon-70x70.png',
+    BASE_PATH + 'images/favicon/ms-icon-144x144.png',
+    BASE_PATH + 'images/favicon/ms-icon-150x150.png',
+    BASE_PATH + 'images/favicon/ms-icon-310x310',
+    
+    //Images for pages
+    
+    //JavaScript
+    BASE_PATH + 'javascript/bulma.js',
+    BASE_PATH + 'javascript/jquery-2.2.0.min.js',
+    BASE_PATH + 'javascript/script.js',
+    
+    //Manifest
+    BASE_PATH + 'manifest.json',
+    
+    //CSS and Fonts
+    BASE_PATH + 'css/bulma.css',
+    BASE_PATH + 'css/styles.css'
 ];
 
 self.addEventListener('install', function(event) {
